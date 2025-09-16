b1208b4e8c89df52a4c9c89d9570de49091f5d9a
safwanp <safwan.woodenclouds@gmail.com>
2025-09-16 16:11:00 +0530

    changes

--- components/common/header.jsx
+++ components/common/header.jsx
@@ -1,12 +1,16 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./button";
 import NAVS from "@/contants/NAVIGATIONS";
 
 const Header = () => {
+  const [isNavActive, setNavActive] = useState(false);
+
   return (
-    <header className="wrapper py-8 flex items-center justify-between">
+    <header className="wrapper py-8 flex items-center justify-between relative">
       <Link href="/">
         <h1 className="flex items-center gap-3">
           <span className="img ">
@@ -17,7 +21,7 @@ const Header = () => {
           </span>
         </h1>
       </Link>
-      <nav className="flex items-center gap-4">
+      <nav className="hidden items-center gap-4 lg:flex">
         <ul className="flex items-center gap-10 text-white text-[16px] font-[500]">
           {NAVS.map((nav, index) => (
             <li key={index}>
@@ -33,6 +37,47 @@ const Header = () => {
           ))}
         </ul>
         <Button>Get Started</Button>
+      </nav>
+      <nav className="lg:hidden ">
+        <span
+          className="hamburger cursor-pointer"
+          onClick={() => setNavActive(true)}
+        >
+          <Image src="/hamburger.svg" alt="hamburger" width={32} height={32} />
+        </span>
+        <div
+          className={`fixed top-0 p-[32px] w-[320px] h-[100vh] bg-[#091C23] ${
+            isNavActive ? "right-0" : "-right-[400px]"
+          } transition-all duration-500`}
+        >
+          <div className="top flex justify-end">
+            <span
+              className="close cursor-pointer"
+              onClick={() => setNavActive(false)}
+            >
+              <Image
+                src="/close-icon.svg"
+                alt="close icon"
+                width={32}
+                height={32}
+              />
+            </span>
+          </div>
+          <ul className="flex flex-col gap-6 text-white text-[16px] font-[500]">
+            {NAVS.map((nav, index) => (
+              <li key={index}>
+                <Link
+                  href={nav.link}
+                  className={`text-white font-poppins font-semibold ${
+                    nav.isActive ? "!text-[#5F82FF]" : ""
+                  }`}
+                >
+                  {nav.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
     </header>
   );
--- components/landing/spotlight.jsx
+++ components/landing/spotlight.jsx
@@ -13,15 +13,15 @@ const Spotlight = () => {
       style={{
         background:
           "linear-gradient(180deg, #091D22 0%, rgba(9, 29, 34, 0) 49.49%, #091D22 100%), linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('/images/spotlight-hero.jpg')",
-        backgroundSize: "100%",
-        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundPosition: "left",
         backgroundRepeat: "no-repeat",
       }}
     >
       <Header />
       <section id="spotlight">
         <div className="wrapper pb-10">
-          <h2 className="text-white text-[78px] mt-[28%] monomaniac-one-regular">
+          <h2 className="text-white text-[78px] sm:text-[42px] md:text-[48px]  mt-[28%] monomaniac-one-regular">
             Ultrices ut etiam vulputate ante congue jokichn na
             <TextAnimation titles={ANIMATED_TITLES} />
           </h2>
